85db1c055819af678f107ce0024de6eaebf418f9
marcolo-30 <maloaiza30@gmail.com>
2026-04-27 13:45:06 +0200

    test: launch CI

--- app/main.py
+++ app/main.py
@@ -73,6 +73,7 @@ class APIHandler(http.server.SimpleHTTPRequestHandler):
             self.send_error(404, 'Not Found')
 
 
+
 # --- Main Execution ---
 if __name__ == "__main__":
     with socketserver.ThreadingTCPServer(('', PORT), APIHandler) as httpd:
